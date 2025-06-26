50e2ea37ac289ef73a17ff3462f1c7d001bdff83
Anayelli <ap319642@gmail.com>
2025-06-26 13:29:02 -0600

    Reapply "Agregó carpeta y vista producto"
    This reverts commit 957f85973654a9450687154a1cb6f1f6e0ce83ec.

---
+++ markettico-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ProductosPage from './pages/ProductosPage';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>MarketTico</h1>
+            <ProductosPage />
+        </div>
+    );
+}
+
+export default App;
