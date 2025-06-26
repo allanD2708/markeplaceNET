957f85973654a9450687154a1cb6f1f6e0ce83ec
Anayelli <ap319642@gmail.com>
2025-06-26 13:28:18 -0600

    Revert "Agregó carpeta y vista producto"
    This reverts commit 555f1a4de197afb02a104c8a894e87800119e9de.

--- markettico-frontend/src/services/ProductoService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'https://localhost:5000/api/producto';
-
-export const obtenerProductos = async () => {
-  const respuesta = await axios.get(API_URL);
-  return respuesta.data;
-};
--- markettico-frontend/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://localhost:5000/api' // ajusta según tu puerto y ruta
-});
-
-export default api;
